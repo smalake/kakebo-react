def029188e5d700e8add5dfc68046ea21b7ed18d
smalake <41889150+smalake@users.noreply.github.com>
2023-04-16 10:50:56 +0900

    Atomを導入

---
+++ src/recoil/EventAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const eventAtom = atom({
+  key: "Event",
+  default: [],
+});
---
+++ src/recoil/PerDayAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const perDayAtom = atom({
+  key: "PerDay",
+  default: [],
+});
