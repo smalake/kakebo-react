4d18ccd8aaea604bd07054fd1e43fd643587a7d1
smalake <41889150+smalake@users.noreply.github.com>
2023-09-12 15:10:02 +0900

    親なら招待URL発行ボタンを表示、子なら非表示にする機能を実装 #8

--- src/api/settingApi.ts
+++ src/api/settingApi.ts
@@ -4,4 +4,5 @@ export const settingApi = {
   getName: () => axiosClient.get("/display-name"),
   updateName: (params: any) => axiosClient.put("display-name", params),
   invite: () => axiosClient.get("/invite"),
+  isParent: () => axiosClient.get("/is-parent"),
 };
