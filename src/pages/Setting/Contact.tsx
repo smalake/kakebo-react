09e38be01e4370619e4ccf9608e78c6befa33dfd
smalake <41889150+smalake@users.noreply.github.com>
2023-10-21 22:27:25 +0900

    お問い合わせフォームの修正

--- src/pages/Setting/Contact.tsx
+++ src/pages/Setting/Contact.tsx
@@ -1,25 +1,44 @@
 import LoadingButton from "@mui/lab/LoadingButton";
 import { Box, TextField } from "@mui/material";
 import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Setting.module.css";
+import { settingApi } from "../../api/settingApi";
 
 export const Contact = () => {
   const navigate = useNavigate();
   const [buttonLoading, setButtonLoading] = useState(false);
 
+  interface SendData {
+    email: string;
+    name: string;
+    description: string;
+  }
+
   // react-hook-formの設定
   const {
     register,
     handleSubmit,
-    setValue,
-    control,
     formState: { errors },
-  } = useForm();
+  } = useForm<SendData>();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: SendData) => {
     setButtonLoading(true);
+    try {
+      const send = {
+        email: data.email,
+        name: data.name,
+        description: data.description,
+      };
+      const res = await settingApi.sendMail(send);
+      if (res.status === 200) {
+        alert("お問い合わせありがとうございます。\n確認した後返信させていただきます。");
+        navigate("/setting");
+      }
+    } catch (err: any) {
+      alert("エラーが発生しました");
+    }
   };
   return (
     <>
@@ -35,14 +54,30 @@ export const Contact = () => {
                 sx={{ width: "90%" }}
                 error={Boolean(errors.name)}
                 {...register("name", { required: "お名前を入力してください" })}
-                // helperText={errors.name?.message}
+                helperText={errors.name?.message}
               />
             </div>
             <div className={styles.form}>
-              <TextField id="emamil" label="メールアドレス" sx={{ width: "90%" }} />
+              <TextField
+                id="emamil"
+                label="返信先のメールアドレス"
+                sx={{ width: "90%" }}
+                error={Boolean(errors.email)}
+                {...register("email", { required: "メールアドレスを入力してください" })}
+                helperText={errors.email?.message}
+              />
             </div>
             <div className={styles.form}>
-              <TextField id="description" label="お問い合わせ内容" sx={{ width: "90%" }} />
+              <TextField
+                id="description"
+                label="お問い合わせ内容"
+                multiline
+                rows={4}
+                sx={{ width: "90%" }}
+                error={Boolean(errors.description)}
+                {...register("description", { required: "お問い合わせ内容を入力してください" })}
+                helperText={errors.description?.message}
+              />
             </div>
             <LoadingButton
               type="submit"
@@ -55,6 +90,9 @@ export const Contact = () => {
             </LoadingButton>
           </form>
         </Box>
+        <Link to="/setting" className={styles.toBack}>
+          戻る
+        </Link>
       </div>
     </>
   );
