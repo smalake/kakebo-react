d3a4441a7f25b987a002bd9760a978feb6ed90c3
smalake <koizumim1002@gmail.com>
2024-06-22 14:40:00 +0900

    認証の有無で別処理にする #63

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,13 +1,14 @@
-import { axiosClient } from "./axiosClient";
+import { axiosAuthedClient } from './axiosAuthedClient';
+import { axiosClient } from './axiosClient';
 
 export const authApi = {
-  login: (params: object) => axiosClient.post("/login", params),
-  logout: () => axiosClient.post("/logout"),
-  register: (params: object) => axiosClient.post("/register", params),
-  googleLogin: (params: object) => axiosClient.post("/google-login", params),
-  join: (params: object) => axiosClient.post("/join", params),
+  login: (params: object) => axiosClient.post('/login', params),
+  logout: () => axiosClient.post('/logout'),
+  register: (params: object) => axiosClient.post('/register', params),
+  googleLogin: (params: object) => axiosClient.post('/google-login', params),
+  join: (params: object) => axiosClient.post('/join', params),
   getName: (group: string) => axiosClient.get(`/get-name/${group}`),
-  isLogin: () => axiosClient.get("/login-check"),
-  verifyEmail: (params: object) => axiosClient.post("/auth-code", params),
-  resendCode: () => axiosClient.get("/resend-code"),
+  isLogin: () => axiosAuthedClient.get('/login-check'),
+  verifyEmail: (params: object) => axiosClient.post('/auth-code', params),
+  resendCode: () => axiosClient.get('/resend-code'),
 };
--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,18 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
-const getToken = () => localStorage.getItem("token") ?? "";
 
 // APIを叩く前に前処理を行う
 axiosClient.interceptors.request.use(async (config: any) => {
   return {
     ...config,
     headers: {
-      "Content-Type": "application/json",
-      authorization: `Bearer ${getToken()}`, //リクエストヘッダーにJWTを付けてサーバに渡す
+      'Content-Type': 'application/json',
     },
   };
 });
